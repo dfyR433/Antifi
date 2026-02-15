d181189eed4ad7028fe670336b2b93200e65b7bf
dfyR433 <213033816+dfyR433@users.noreply.github.com>
2026-02-15 06:13:05 -0800

    Add files via upload

---
+++ Antifi/beacon.h
@@ -0,0 +1,28 @@
+#ifndef BEACON_H
+#define BEACON_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
+#include "esp_err.h"
+
+#define BEACON_FRAME_SIZE 128
+#define MAX_SSID_LEN 32
+
+extern bool beacon_active;
+extern uint8_t beacon_frame[BEACON_FRAME_SIZE];
+extern uint8_t current_channel;
+extern uint32_t packet_counter;
+extern uint32_t start_time;
+extern const int NUM_SSIDS;
+
+void beacon_setup();
+void beacon_loop();
+void start_beacon();
+void stop_beacon();
+bool is_beacon_active();
+void send_beacon(int ssid_index);
+void generate_mac(uint8_t* mac, int index);
+
+#endif
